4590bea16b1d8c8175e4a503a734dc74284d3426
Ilia642021 <ilia.a.yakovenko@gmail.com>
2026-06-07 22:45:08 +0400

    Доработка отчета по курсовому проекту (Вариант 8)
    - Обновлены метрики задержки (TCP End-to-End Delay) и RTT во всех таблицах и тексте на основе актуальных данных моделирования.
    - Добавлен раздел «Требования к оборудованию по уровням иерархии» с функциональными требованиями.
    - Добавлено текстовое описание физической среды передачи данных и обоснование выбора технологий.
    - Добавлен подраздел «Обоснование генерируемого трафика» с теоретическими расчетами.
    - Расширен отчет по событиям имитационного моделирования.
    - Усилен анализ влияния резервного канала связи (индивидуальное задание).
    - Расширена теоретико-расчетная часть новыми подразделами и формулами.
    - Добавлена подробная физическая схема сети (detailed_physical_scheme.png).
    - Исправлены нестыковки в OSPF cost и времени сходимости.
    - Подтверждено отсутствие потерь пакетов в сценарии C.

--- scripts/run_all_sims.sh
+++ scripts/run_all_sims.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-export PATH=$PATH:/home/dev/6_sem/KURSACH_seti/02_model/omnetpp-6.0.3/bin
-export LD_LIBRARY_PATH=/home/dev/6_sem/KURSACH_seti/02_model/inet4.5/src:$LD_LIBRARY_PATH
+echo "⚠️  ПРЕДУПРЕЖДЕНИЕ: Этот скрипт запускает полный перерасчет всех симуляционных данных!"
+echo "Это приведет к сбросу всех существующих результатов, которые вам нужно будет обновить в отчете заново."
+echo "Если вы действительно уверены, что хотите перезапустить все симуляции, отредактируйте скрипт чтобы снять блокировку."
+exit 1
 
-cd /home/dev/6_sem/KURSACH_seti/02_model/kursach_model
-
-configs=("A_Base" "B_ReserveEnabled" "C_FailurePrimary" "D_ScaleUp")
-
-for config in "${configs[@]}"
-do
-    echo "Running config: $config..."
-    opp_run -u Cmdenv -c $config -n .:../inet4.5/src:../inet4.5/tutorials:../inet4.5/showcases --image-path=../inet4.5/images -l ../inet4.5/src/INET omnetpp.ini > "../../03_results/log_${config}.txt" 2>&1
-    echo "Done $config."
-done
+# Раскомментируйте код ниже только если вы точно хотите перезапустить расчеты!
+# export PATH=$PATH:/home/dev/6_sem/KURSACH_seti/02_model/omnetpp-6.0.3/bin
+# export LD_LIBRARY_PATH=/home/dev/6_sem/KURSACH_seti/02_model/inet4.5/src:$LD_LIBRARY_PATH
+# 
+# cd /home/dev/6_sem/KURSACH_seti/02_model/kursach_model
+# 
+# configs=("A_Base" "B_ReserveEnabled" "C_FailurePrimary" "D_ScaleUp")
+# 
+# for config in "${configs[@]}"
+# do
+#     echo "Running config: $config..."
+#     opp_run -u Cmdenv -c $config -n .:../inet4.5/src:../inet4.5/tutorials:../inet4.5/showcases --image-path=../inet4.5/images -l ../inet4.5/src/INET omnetpp.ini > "../../03_results/log_${config}.txt" 2>&1
+#     echo "Done $config."
+# done
